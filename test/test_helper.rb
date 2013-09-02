6168200f7c2b8bb289d383a80d16fecca7eb2926
Ben Johnson <benbjohnson@yahoo.com>
2013-09-02 15:42:38 -0600

    Add row normalization.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,9 @@ require 'webmock/minitest'
 require 'unindentable'
 require 'em-http'
 require 'skydb'
+
+class MiniTest::Unit::TestCase
+  def fixture(path)
+    File.join(File.expand_path('../fixtures', __FILE__), path)
+  end
+end
